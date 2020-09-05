6af5b39ee855d58ffe82f98bc3b85f7d5f0a62b2
open_2_code <jwgoode@gmail.com>
2020-09-05 12:30:17 -0400

    "fixed post route session- new vowner view"

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,37 +1,25 @@
 class SessionsControllersController < ApplicationController
 
   # GET: /sessions_controllers
-  get "/sessions_controllers" do
-    erb :"/sessions_controllers/index.html"
+  get "/login" do
+    erb :"/sessions/login.html"
   end
 
-  # GET: /sessions_controllers/new
-  get "/sessions_controllers/new" do
-    erb :"/sessions_controllers/new.html"
-  end
+  
 
   # POST: /sessions_controllers
-  post "/sessions_controllers" do
-    redirect "/sessions_controllers"
-  end
-
-  # GET: /sessions_controllers/5
-  get "/sessions_controllers/:id" do
-    erb :"/sessions_controllers/show.html"
-  end
-
-  # GET: /sessions_controllers/5/edit
-  get "/sessions_controllers/:id/edit" do
-    erb :"/sessions_controllers/edit.html"
-  end
-
-  # PATCH: /sessions_controllers/5
-  patch "/sessions_controllers/:id" do
-    redirect "/sessions_controllers/:id"
-  end
-
-  # DELETE: /sessions_controllers/5/delete
-  delete "/sessions_controllers/:id/delete" do
-    redirect "/sessions_controllers"
+  post "/login" do
+    owner = Owner.find_by_email(params[:email])
+    # if they typed in the right password then log them in, if not show them the form again
+    if owner && owner.authenticate(params[:password]) 
+      session[:id] = owner.id
+      redirect "/"
+    else 
+      @error = "Incorrect email or password"
+      erb :'/sessions/login'
+    end
   end
 end
+
+  
+
